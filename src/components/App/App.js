5b1e81b35e0bc49967cdc9e2339876a308f0cb07
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-03-08 00:26:29 +0200

    pre-finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,78 +1,48 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from 'components/ContactForm';
 import { GlobalStyle } from 'constants/GlobalStyles';
 import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/Filter';
 import { Container, Wrap, MainTitle, Title, StyleArround } from './App.styled';
 
-const INITIAL_STATE = {
-  // contacts: [
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ],
-  contacts: [],
-  filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChangeName = e => setFilter(e.currentTarget.value);
+
+  const handleChange = contact => {
+    setContacts(prevContacts => [...prevContacts, contact]);
+  };
+
+  const handleFilter = () =>
+    contacts.filter(({ name }) =>
+      name.toLowerCase().trim().includes(filter.toLowerCase().trim())
+    );
+
+  const handleDelete = id =>
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+
+  return (
+    <Container>
+      <Wrap>
+        <MainTitle>Phonebook</MainTitle>
+        <ContactForm onChange={handleChange} contacts={contacts} />
+
+        <Title>Contacts</Title>
+        <Filter filter={filter} onChangeName={handleChangeName} />
+        <ContactList filteredArr={handleFilter()} onDelete={handleDelete} />
+
+        <GlobalStyle></GlobalStyle>
+      </Wrap>
+      <StyleArround />
+    </Container>
+  );
 };
-
-export class App extends Component {
-  state = { ...INITIAL_STATE };
-
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts !== null) {
-      this.setState({ contacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevContacts = prevState.contacts;
-    const nextContacts = this.state.contacts;
-    if (prevContacts !== nextContacts) {
-      localStorage.setItem('contacts', JSON.stringify(nextContacts));
-    }
-  }
-
-  handleChangeName = e => this.setState({ filter: e.currentTarget.value });
-
-  handleChange = data => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data],
-    }));
-  };
-
-  handleFilter = () => {
-    return this.state.contacts.filter(({ name }) =>
-      name.toLowerCase().trim().includes(this.state.filter.toLowerCase().trim())
-    );
-  };
-
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const { filter, contacts } = this.state;
-    return (
-      <Container>
-        <Wrap>
-          <MainTitle>Phonebook</MainTitle>
-          <ContactForm onChange={this.handleChange} contacts={contacts} />
-
-          <Title>Contacts</Title>
-          <Filter filter={filter} onChangeName={this.handleChangeName} />
-          <ContactList
-            filteredArr={this.handleFilter()}
-            onDelete={this.handleDelete}
-          />
-
-          <GlobalStyle></GlobalStyle>
-        </Wrap>
-        <StyleArround />
-      </Container>
-    );
-  }
-}
